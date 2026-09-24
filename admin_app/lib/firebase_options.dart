419a75b7998e96bc76de00282416d3028d6402b1
rohitfcg123-arch <rohit.fcg123@gmail.com>
2026-09-24 20:44:20 +0530

    Add CMA Admin Portal app foundation

---
+++ admin_app/lib/firebase_options.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+class DefaultFirebaseOptions {
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyDPNLtekyLhpnZIDmkFeVKji1ozUKdnyfQ',
+    appId: '1:208738737302:android:ea0410ab60d22ce5fef6b7',
+    messagingSenderId: '208738737302',
+    projectId: 'cma-mcq-portal-cf33f',
+    storageBucket: 'cma-mcq-portal-cf33f.firebasestorage.app',
+  );
+  static FirebaseOptions get currentPlatform => android;
+}
